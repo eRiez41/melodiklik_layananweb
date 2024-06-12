76553a6b81528fbcd0ee43d9575f0405b26476d4
eRiez41 <dandibaret74@gmail.com>
2024-06-12 10:50:22 +0700

    merevisi halaman admin

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,25 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/dashboard/seller', [SellerDashboardController::class, 'index'])->name('seller.dashboard');
 Route::get('/dashboard/admin', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
 
+// Route::get('/dashboard/admin', function () {
+//     return view('admin.dashboard');
+// })->name('admin.dashboard')->middleware('auth');
+
+Route::get('admin/users', function () {
+    return view('admin.users');
+});
+
+Route::get('admin/sellers', function () {
+    return view('admin.sellers');
+});
+
+Route::get('admin/products', function () {
+    return view('admin.products');
+});
+
+Route::get('admin/transactions', function () {
+    return view('admin.transactions');
+});
 
 Route::get('/barang', [BarangController::class, 'index'])->name('barang.index');
 
