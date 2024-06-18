edec4f1acbf15cdd4a5447e863c02334c6507643
eRiez41 <dandibaret74@gmail.com>
2024-06-18 18:36:16 +0700

    menambah modal profil, relayouting navbar

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -1,16 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wishlist</title>
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div class="container mt-5">
-        <h1>Wishlist</h1>
-        <!-- Tambahkan konten wishlist di sini -->
-        <p>Ini adalah halaman wishlist.</p>
+@extends('layouts.app')
+
+@section('title', 'Wishlist')
+
+@section('content')
+<div class="container">
+    <h1>Wishlist</h1>
+    <div class="row">
+        @foreach ($wishlistItems as $item)
+            <div class="col-md-3">
+                <div class="card mb-4">
+                    <img src="{{ $item['image'] }}" class="card-img-top" alt="{{ $item['name'] }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $item['name'] }}</h5>
+                        <p class="card-text">Rp {{ number_format($item['price'], 0, ',', '.') }}</p>
+                        <a href="{{ route('barang.show', $item['id']) }}" class="btn btn-primary">Lihat Produk</a>
+                        <a href="#" class="btn btn-danger">Hapus</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
-</body>
-</html>
+</div>
+@endsection
